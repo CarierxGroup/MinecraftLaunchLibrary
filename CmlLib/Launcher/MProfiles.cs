3dfb8a180b4c0e8688c801ad1de2c88c8070ef7f
晚餐骨Dinnerbone <3293876954@qq.com>
2019-10-27 08:58:20 +0800

    Made In China

---
+++ CmlLib/Launcher/MProfiles.cs
@@ -0,0 +1,181 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CmlLib.Launcher
+{
+    public class MProfile
+    {
+        // 프로파일 파싱
+        public static MProfile Parse(MProfileInfo info)
+        {
+            string json;
+            if (info.IsWeb)
+            {
+                using (var wc = new WebClient())
+                {
+                    json = wc.DownloadString(info.Path);
+                    return ParseFromJson(json);
+                }
+            }
+            else
+                return ParseFromFile(info.Path);
+        }
+
+        public static MProfile ParseFromFile(string path)
+        {
+            var json = File.ReadAllText(path);
+            return ParseFromJson(json);
+        }
+
+        private static MProfile ParseFromJson(string json)
+        {
+            var profile = new MProfile();
+            var job = JObject.Parse(json);
+            profile.Id = job["id"]?.ToString();
+
+            var assetindex = (JObject)job["assetIndex"];
+            if (assetindex != null)
+            {
+                profile.AssetId = n(assetindex["id"]?.ToString());
+                profile.AssetUrl = n(assetindex["url"]?.ToString());
+                profile.AssetHash = n(assetindex["sha1"]?.ToString());
+            }
+
+            var client = job["downloads"]?["client"];
+            if (client != null)
+            {
+                profile.ClientDownloadUrl = client["url"]?.ToString();
+                profile.ClientHash = client["sha1"]?.ToString();
+            }
+
+            profile.Libraries = MLibrary.ParseJson((JArray)job["libraries"]);
+            profile.MainClass = n(job["mainClass"]?.ToString());
+
+            var ma = job["minecraftArguments"]?.ToString();
+            if (ma != null)
+                profile.MinecraftArguments = ma;
+            var ag = job["arguments"]?.ToString();
+            if (ag != null)
+                profile.Arguments = ag;
+
+            profile.ReleaseTime = job["releaseTime"]?.ToString();
+
+            var ype = job["type"]?.ToString();
+            profile.TypeStr = ype;
+            switch (ype)
+            {
+                case "release":
+                    profile.Type = MProfileType.Release;
+                    break;
+                case "snapshot":
+                    profile.Type = MProfileType.Snapshot;
+                    break;
+                case "old_alpha":
+                    profile.Type = MProfileType.OldAlpha;
+                    break;
+                default:
+                    profile.Type = MProfileType.Unknown;
+                    break;
+            }
+
+            if (job["jar"] != null)
+            {
+                profile.IsForge = true;
+                profile.InnerJarId = job["jar"].ToString();
+            }
+
+            var path = Minecraft.Versions + profile.Id;
+            Directory.CreateDirectory(path);
+            File.WriteAllText(path + "\\" + profile.Id + ".json", json);
+
+            return profile;
+        }
+
+        public void WriteProfile()
+        {
+
+        }
+
+        static string n(string t)
+        {
+            return (t == null) ? "" : t;
+        }
+
+        public void ChangeAssets(string Id, string Url)
+        {
+            AssetId = Id; AssetUrl = Url;
+        }
+
+        /// <summary>
+        /// true 이면 모장 서버에 있는 프로파일, false 이면 로컬에 있는 프로파일
+        /// </summary>
+        public bool IsWeb { get; private set; }
+        /// <summary>
+        /// true 이면 포지 프로파일
+        /// </summary>
+        public bool IsForge { get; private set; } = false;
+
+        /// <summary>
+        /// 프로파일의 id
+        /// </summary>
+        public string Id { get; private set; } = "";
+
+        /// <summary>
+        /// 인덱스 파일의 ID
+        /// </summary>
+        public string AssetId { get; private set; } = "";
+        /// <summary>
+        /// 인덱스 파일의 경로
+        /// </summary>
+        public string AssetUrl { get; private set; } = "";
+        public string AssetHash { get; private set; } = "";
+
+        /// <summary>
+        /// 게임 다운로드 경로
+        /// </summary>
+        public string ClientDownloadUrl { get; private set; } = "";
+        public string ClientHash { get; private set; } = "";
+        /// <summary>
+        /// 라이브러리 리스트
+        /// </summary>
+        public MLibrary[] Libraries { get; private set; }
+        /// <summary>
+        /// 메인 클래스
+        /// </summary>
+        public string MainClass { get; private set; } = "";
+        /// <summary>
+        /// 실행 인수 (1.3 이전에 나온 버전)
+        /// </summary>
+        public string MinecraftArguments { get; private set; } = "";
+        /// <summary>
+        /// 실행 인수 JSON (1.3 부터 이후 버전)
+        /// </summary>
+        public string Arguments { get; private set; } = "";
+        /// <summary>
+        /// 프로파일의 생성된 날짜
+        /// </summary>
+        public string ReleaseTime { get; private set; } = "";
+        /// <summary>
+        /// 프로파일의 종류
+        /// </summary>
+        public MProfileType Type { get; private set; } = MProfileType.Unknown;
+
+        public string TypeStr { get; private set; } = "";
+
+        /// <summary>
+        /// 포지 프로파일일때 베이스가 되는 프로파일의 ID
+        /// </summary>
+        public string InnerJarId { get; private set; } = "";
+        /// <summary>
+        /// 네이티브 라이브러리가 저장되 있는 경로
+        /// </summary>
+        public string NativePath { get; set; } = "";
+    }
+}
---
+++ README.md
@@ -0,0 +1,162 @@
+﻿Minecraft Launcher Library
+======================
+
+### Online / Offline Login, Download, Launch with various options, Forge Support
+
+### Support All Versions, Forge
+### Sample Project(not completed) : https://github.com/AlphaBs/AlphaMinecraftLauncher  
+  
+
+Contacts
+-------------
+
+3293876594@qq.com
+
+License
+--------------
+Nothing
+
+Dependancy
+-------------
+Newtonsoft.Json
+DotNetZip
+
+Use Nuget-Restore
+
+How To Use
+-------------
+
+*Sorry about my poor english skill*
+
+If you want to learn more like java runtime download, detail launch options, full methods, go to [wiki](https://merong)
+
+#### 1. Prepare
+Open the 'CmlLib' Project, restore nuget packages, build CmlLib Project.
+and add reference to 'CmlLib.dll', 'Newtonsoft.Json.dll', 'DotNetZip.dll' to your own project.
+
+write this on the top of your source code.
+
+
+      using CmlLib.Launcher;
+
+#### 2. Minecraft Initialize
+You should write this code before work.
+
+      Minecraft.Initialize("GAME_DIRECTORY");
+
+It set Game Directory that is used to download game files, load profiles, save login session, Launch, etc...  
+**DO NOT USE Relative Path. Only use abstract path.**
+
+#### 3. Login
+
+     MLogin login = new MLogin();
+     MSession session = null;
+
+     session = login.TryAutoLogin();
+     if (session.Result != MLoginResult.Success)
+     {
+          session = login.Authenticate(
+               "YOUR_MOJANG_EMAIL",
+               "PASSWORD");
+
+          if (session.result != MLoginResult.Success)
+               throw new Exception("Wrong Account");
+     }
+
+     Console.WriteLine("Hello, " + session.Username);
+
+The 'session' is login result.
+if you want to connect online-mode server, you use this session to launch.
+note : you can't use old login which use username instead mojang email.
+
+or you can use offline session :
+
+     MSession session = MSession.GetOfflineSession("USERNAME");
+
+#### 4. Get Profile Infos
+Profile contain various data which launcher need.
+All Game Versions has its own profile, even old alpha version or forge.
+You can find at (GameDirectory)￦versions￦(any-version)￦(version-name).json.
+MProfileInfo is Profile's Metadata, contains Name, Profile Path(Url), Type(Release, Snapshot, Old), ReleaseTime.
+and this code get profile info :
+
+     MProfilesInfo[] infos = MProfileInfo.GetProfiles();
+
+or you can choose source :
+
+     // get profiles from mojang server
+     var web = MProfileInfo.GetProfilesFromWeb();
+     // get profiles from game directory
+     var local = MProfileInfi.GetProfilesFromLocal();
+
+#### 5. Choose ProfileInfo and Parse.
+
+In order to use profile's data, you should parse Profile from ProfileInfo.
+
+First, Search Profile Infos you want to launch : (very simple search algorithm)
+
+     MProfile profile = null;
+     foreach (var item in infos)
+     {
+          if (item.Name == "1.7.10")
+          {
+                profile = item;
+                break;
+          }
+     }
+
+Parse Profile is simple :
+
+     MProfile profile = MProfile.Parse(info);
+
+#### 6. Check & Download Game Files
+
+     MDownloader downloader = new MDownloader(profile);
+     downloader.ChangeFileChange += change_file;
+     downloader.ChangeProgressChange += change_progress;
+     downloader.DownloadAll();
+
+ChangeFileChange Event : Change Download File Name
+
+ChangeProgressChange : Change One File's Download Progress
+
+DownloadAll : Check And Download All Game Files
+
+DownloadAll() Method do this :
+
+     // downloader.DownloadAll() do this :
+     
+     downloader.DownloadLibrary(); // libraries
+     downloader.DownloadIndex(); // asset index
+     downloader.DownloadResource(); // assets / resources
+     downloader.DownloadMinecraft(); // game jar
+
+It's so long. so just use DownloadAll();
+
+Each Download~~ method check game file exist, and if file doesn't exist, download game file from mojang server.
+
+#### 7. Make game args and Launch
+
+     var option = new MLaunchOption()
+     {
+          // must require
+          StartProfile = profile,
+          JavaPath = "java.exe", //SET YOUR JAVA PATH (if you want autoset, goto wiki)
+          MaximumRamMb = int.Parse(xmx),
+          Session = session,
+          
+          // not require
+          ServerIP = "", // connect server directly
+          LauncherName = "", // display launcher name at main window
+          CustomJavaParameter = "" // set your own java args
+     };
+     
+     var launch = new MLaunch(option);
+     var process = launch.MakeProcess();
+     process.Start();
+
+Set launch options, and Launch it!
+
+#### 8. Launch Forge
+
+goto [wiki](https://merong)
